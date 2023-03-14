49838ef719002d8116848695f0d57630a53f2d1d
Boheon <86608286+Boheon@users.noreply.github.com>
2023-03-14 22:02:19 +0900

    해쉬테이블 추가

---
+++ src/HashTables/Main.java
@@ -0,0 +1,10 @@
+package HashTables;
+
+public class Main {
+    public static void main(String[] args) {
+
+        HashTable myHashTable = new HashTable();
+
+        myHashTable.printTable();
+    }
+}
