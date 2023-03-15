9e1043d4fb4a46e1a487463abe576725af3f8654
Boheon <86608286+Boheon@users.noreply.github.com>
2023-03-15 16:31:39 +0900

    해쉬테이블 get추가

--- src/HashTables/HashTable.java
+++ src/HashTables/HashTable.java
@@ -54,5 +54,15 @@ public class HashTable {
         }
     }
 
+    public int get(String key){
+         int index = hash(key);
+         Node temp = dataMap[index];
+         while(temp != null){
+              if(temp.key == key) return temp.value;
+              temp = temp.next;
+         }
+         return 0;//key가 없을 경우
+    }
+
 
 }
