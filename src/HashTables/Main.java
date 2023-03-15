83362193d87b46ad0c219c988afa02b6ec05a7aa
Boheon <86608286+Boheon@users.noreply.github.com>
2023-03-15 17:43:54 +0900

    해쉬테이블 공통 원소찾기 추가

--- src/HashTables/Main.java
+++ src/HashTables/Main.java
@@ -1,6 +1,20 @@
 package HashTables;
 
+import java.util.HashMap;
+
 public class Main {
+    public static boolean itemInCommon(int[] array1, int[] array2){
+        HashMap<Integer, Boolean> myHashMap = new HashMap<>();
+
+        for(int i : array1){
+            myHashMap.put(i, true);
+        }
+
+        for(int j : array2){
+            if(myHashMap.get(j) != null) return true;
+        }
+        return false;
+    }
     public static void main(String[] args) {
 
         HashTable myHashTable = new HashTable();
