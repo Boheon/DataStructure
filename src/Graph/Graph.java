53e6f621ce13fceb9b983a0b33497e47f0e0ce4b
Boheon <86608286+Boheon@users.noreply.github.com>
2023-03-18 17:02:15 +0900

    그래프 add vertex 추가

--- src/Graph/Graph.java
+++ src/Graph/Graph.java
@@ -5,6 +5,11 @@ import java.util.HashMap;
 
 public class Graph {
     private HashMap<String, ArrayList<String>> adjList = new HashMap<>();
+
+   public void printGraph(){
+       System.out.println(adjList);
+   }
+
     public boolean addVertex(String vertex){
         if(adjList.get(vertex) == null) {
             adjList.put(vertex, new ArrayList<String>());
