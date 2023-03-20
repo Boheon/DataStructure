257824c6355fae8f47589b46fb1cddeb7ef6a675
Boheon <86608286+Boheon@users.noreply.github.com>
2023-03-20 18:26:48 +0900

    그래프 remove edge 추가

--- src/Graph/Main.java
+++ src/Graph/Main.java
@@ -3,5 +3,20 @@ package Graph;
 public class Main {
     public static void main(String[] args) {
 
+        Graph myGraph = new Graph();
+
+        myGraph.addVertex("A");
+        myGraph.addVertex("B");
+        myGraph.addVertex("C");
+
+        myGraph.addEdge("A","B");
+        myGraph.addEdge("A","C");
+        myGraph.addEdge("B","C");
+
+        myGraph.removeEdge("A", "B");
+
+
+        myGraph.printGraph();
+
     }
 }
