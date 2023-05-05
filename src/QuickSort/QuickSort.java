1a24f44021b4053d834a8836bce776a294bc5416
Boheon <86608286+Boheon@users.noreply.github.com>
2023-05-05 16:40:38 +0900

    QuickSort추가

---
+++ src/QuickSort/QuickSort.java
@@ -0,0 +1,50 @@
+package QuickSort;
+
+import java.util.Arrays;
+
+public class QuickSort {
+    private static void swap(int[] array, int firstIndex, int secondIndex){
+        int temp = array[firstIndex];
+        array[firstIndex] = array[secondIndex];
+        array[secondIndex] = temp;
+    }
+
+    private static int pivot(int[] array, int pivotIndex, int endIndex){
+        int swapIndex = pivotIndex;
+        for(int i = pivotIndex + 1; i <=endIndex; i++){
+            if(array[i] < array[pivotIndex]){
+                swapIndex++;
+                swap(array, swapIndex, i);
+            }
+        }
+        swap(array, pivotIndex, swapIndex);
+
+        return swapIndex;
+    }
+
+    public static void quickSort(int[] array, int left, int right){
+        if(left < right) {
+            int pivotIndex = pivot(array, left, right);
+            quickSort(array, left, pivotIndex - 1);
+            quickSort(array, pivotIndex + 1, right);
+        }
+    }
+
+    public static void quickSort(int[] array){
+        quickSort(array, 0, array.length-1);
+    }
+
+    public static void main(String[] args) {
+        int[] myArray = {4,6,1,7,3,2,5};
+        int returnedIndex = pivot(myArray, 0, 6);
+
+        System.out.println( "Returned Index: " + returnedIndex);
+        System.out.println(Arrays.toString(myArray));
+
+        //quickSort(myArray, 0, 6);
+        quickSort(myArray);
+
+        System.out.println(Arrays.toString(myArray));
+
+    }
+}
